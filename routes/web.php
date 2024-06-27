f47d8ace76b816e60aa225161bb0c099d8ce04bc
xeryz05 <xierradepan01@gmail.com>
2024-06-27 17:24:23 +0700

    create home page

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/home', function () {
+    return view('home');
+});
 
 // Route::get('quran',[QuranController::class, 'index'])->name('quran');
 Route::get('/',[QuranController::class,'index'])->name('/');
